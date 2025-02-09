2ae903ff7dd7595805417fda59d60e3d28ad1793
PawelPiech1983 <pawel.piech1983@gmail.com>
2025-02-09 21:22:55 +0100

    6.2 Added calculator in kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,21 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
 
         System.out.println("6.2 Testowanie kalkulatora");
         Calculator calculator = new Calculator();
@@ -14,25 +25,25 @@ public class TestingMain {
             System.out.println("Dodawanie kalkulatora przeszło pozytywny test");
         }
         else {
-            System.out.println("Dodawanie kalkulatora nie przeszło pozysytwnie testy");
+            System.out.println("Dodawanie kalkulatora nie przeszło pozytywnie testu");
         }
         if (calculator.calculateSubtraction(numberA, numberB) == numberA + numberB) {
             System.out.println("Odejmowanie kalkulatora przeszło pozytywny test");
         }
         else {
-            System.out.println("Odejmowanie kalkulatora nie przeszło pozysytwnie testy");
+            System.out.println("Odejmowanie kalkulatora nie przeszło pozytywnie testu");
         }
         if (calculator.calculateMultiplication(numberA, numberB) == numberA * numberB) {
             System.out.println("Mnożenie kalkulatora przeszło pozytywny test");
         }
         else {
-            System.out.println("Mnożenie kalkulatora nie przeszło pozysytwnie testy");
+            System.out.println("Mnożenie kalkulatora nie przeszło pozytywnie testu");
         }
         if (calculator.calculateDivision(numberA, numberB) == numberA / numberB) {
             System.out.println("Dzielenie kalkulatora przeszło pozytywny test");
         }
         else {
-            System.out.println("Dzielenie kalkulatora nie przeszło pozysytwnie testy");
+            System.out.println("Dzielenie kalkulatora nie przeszło pozytywnie testu");
         }
 
     }
